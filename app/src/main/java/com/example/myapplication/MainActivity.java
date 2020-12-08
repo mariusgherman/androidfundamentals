c4f52e6a250969f7b09c299fa0f77db12da7a319
marius.gherman <marius.gherman@8x8.com>
2020-12-08 18:14:58 +0200

    base for

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,37 +1,14 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements
-        Listener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        HelloFragment firstFragment = new HelloFragment();
-        Bundle firstBundle = new Bundle();
-        firstBundle.putString("message", "This is my first message");
-        firstFragment.setArguments(firstBundle);
-
-        HelloFragment secondFragment = new HelloFragment();
-        Bundle secondBundle = new Bundle();
-        secondBundle.putString("message", "This is my second message");
-        secondFragment.setArguments(secondBundle);
-
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.first_frame, firstFragment, "Hello 1")
-                .add(R.id.second_frame, secondFragment, "Hello 2")
-                .commit();
     }
 
-    @Override
-    public void onButtonPressed() {
-        HelloFragment secondFragment =
-                (HelloFragment) getSupportFragmentManager().findFragmentByTag("Hello 2");
-        secondFragment.hideYourButton();
-    }
 }
