bf4fdfbf6c32b85604f01db37bf9ffed8cffb9f9
marius.gherman <marius.gherman@8x8.com>
2021-01-21 11:21:36 +0200

    styles app extension

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -27,6 +36,37 @@ public class MainActivity extends AppCompatActivity implements PersonListener {
 
         DataSource dataSource = new DataSource(this);
         dataSource.getPersons(this);
+
+        findViewById(R.id.floating).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) !=
+                        PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 1000);
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_LONG).show();
+                    // do smth with camera
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1000) {
+            switch (permissions[0]) {
+                case Manifest.permission.CAMERA:
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        // do smth with camera
+                    }
+                    break;
+            }
+
+        }
     }
 
     @Override
